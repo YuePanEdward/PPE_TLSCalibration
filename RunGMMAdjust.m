c1601caaa130278f1d68f0ea116f1b3fab8b7a0d
YuePanEdward <2015301610150@whu.edu.cn>
2020-10-16 02:53:19 +0200

    add code structure

--- RunGMMAdjust.m
+++ RunGMMAdjust.m
@@ -15,13 +15,12 @@ op_count=size(ops,1);           %N
 unknown_count=ap_count+6*scan_count;   %u
 ob_count=3*op_count*can_count;  %n
 
-% Pre-Allocation
+
 iter_count = 0;
-d_xi = inf;
-crit = 1;
+is_converged =0;
 
 % Iteration % TODO:
-while crit && (iter_count < adjustment_data_struct.max_iter_count)
+while (iter_count < adjustment_data_struct.max_iter_count)
     iter_count = iter_count + 1;
     
     % Caculate A matrix (Jocubians) by numerical derivative 
@@ -30,32 +29,17 @@ while crit && (iter_count < adjustment_data_struct.max_iter_count)
     for i = 1:scan_count
        for j = 1:op_count
           for k = 1:ap_count % calculate derivative for APs
-              ObsFunction(op, sp, aps, eps)
-               
-               A(k:k+2,k) = derivative(@funcModel,q,xi(1:4),xi(j*6-1:j*6+4),OP{j}(i,:),scans{j}(i,:),dt(q));
+              cur_res = ObsFunction(op, sp, aps, eps);
+              A_mat() =  Derivative(cur_res, dt);
+      
           end
-
-           for q = (4+ 6*j-5):(4+ 6*j) % calculate derivative for EPs
-               A(k:k+2,q) = derivative(@funcModel,(q-6*(j-1)),xi(1:4),xi(j*6-1:j*6+4),OP{j}(i,:),scans{j}(i,:),dt(q));
-           end
-           k = k + 3;
-    end
-    
-    
-    A = zeros(3*sum([n{:}]),4+6*s);
-    k = 1;
-    for j = 1:s
-       for i = 1:n{j}
-           for q = 1:4
-               A(k:k+2,q) = derivative(@funcModel,q,xi(1:4),xi(j*6-1:j*6+4),OP{j}(i,:),scans{j}(i,:),dt(q));
-           end
-
-           for q = (4+ 6*j-5):(4+ 6*j)
-               A(k:k+2,q) = derivative(@funcModel,(q-6*(j-1)),xi(1:4),xi(j*6-1:j*6+4),OP{j}(i,:),scans{j}(i,:),dt(q));
-           end
-           k = k + 3;
+          for k = (6*i-1):(6*i+4) % calculate derivative for EPs
+              cur_res = ObsFunction(op, sp, aps, eps);
+              A_mat() =  Derivative(cur_res, dt);
+          end
        end
-
     end
+    %TODO
+    
 
 end
